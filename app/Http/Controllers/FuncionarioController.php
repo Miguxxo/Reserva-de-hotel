ca7bb4f6b7999cd01c329eca3755a013efed1c27
Miguxxo <igormarcelo93@hotmail.com>
2024-02-16 22:52:00 -0300

    cadastroClienteFuncionario

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ClienteController extends Controller
+class FuncionarioController extends Controller
 {
     public function showHome(){
         return view("home");
     }
 
     public function showFormularioCadastro(Request $request){
-        return view("formulariocadastroCliente");
+        return view("formulariocadastroFuncionario");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ClienteController::class,'showHome']);
+Route::get('/cadastro-cliente',[ClienteController::class,'showFormularioCadastro']);
+Route::get('/cadastro-funcionario',[ClienteController::class,'showFormularioCadastro']);
